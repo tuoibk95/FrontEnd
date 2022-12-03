e45cb3a3cf7070a769b51821c048053cf8689098
tuoibk95 <tuoith95@gmail.com>
2022-12-03 11:25:54 +0700

    commit switch css

--- angularFirst/src/app/app.module.ts
+++ angularFirst/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GreetComponent } from './greet/greet.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GreetComponent
   ],
   imports: [
     BrowserModule
