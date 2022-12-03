5561fb396fd1576804a0f95d953077fc9e710107
tuoibk95 <tuoith95@gmail.com>
2022-12-03 10:44:17 +0700

    commit

---
+++ angularFirst/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+
+// decorrator
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angularFirst';
+}
